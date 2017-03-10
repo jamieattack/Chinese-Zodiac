1ed0222b7aff1faf77fe48844bf5c572ad577067
Jamie Cooper <jamieattack@hotmail.com>
2017-03-09 21:08:51 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+prompt("What is your sign?")
+
+var signArray = [
+	{
+		sign: "dog",
+		image: img/dog.jpg,
+		reading:"beware"
+	},
+	{
+		sign: "dragon",
+		image: img/dragon.jpg,
+		reading:"beware"
+	},
+	{
+		sign: "goat",
+		image: img/goat.jpg,
+		reading:"beware"
+	}
+]
+
+for( i = 0; i = signArray ; i++){
+	// 
+	if(prompt("dog")) {
+		//
+		.innerHTML(signArray[i].image + " " + signArray[i].purchased)
+	}
+}
